683da88ce24effc6f0428fd6a380b79a1fa1b7ea
mnanfack <mnanfack@irex.aretex.ca>
2025-04-24 03:55:13 +0100

    Initial Spring boot Structure

---
+++ platform/src/test/java/cm/amcloud/platform/ReservationPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package cm.amcloud.platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReservationPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
